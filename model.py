79876386aa00ba77971d70d4fe39c7ea0c9f3461
Hackbright Student <info@hackbrightacademy.com>
2014-06-30 18:19:14 -0700

    wrote function to load users and movies

--- model.py
+++ model.py
@@ -1,13 +1,52 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    age = Column(Integer, nullable = True)
+    gender = Column(String(1), nullable = True)
+    occupation = Column(String(20), nullable = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
 
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    title = Column(String(80), nullable = False)
+    date = Column(DateTime, nullable = False)
+    imdb = Column(String(140), nullable = False)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = False)
+    user_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,13 +1,50 @@
-import model
+from model import User, Movie, Rating, connect
 import csv
+from datetime import datetime
 
 def load_users(session):
-    # use u.user
-    pass
+    user_file = open('seed_data/u.user').read().split('\n')
+    for line in user_file:
+        split_line = line.split('|')
+        if len(split_line) == 5:
+            user_id = split_line[0]
+            age = split_line[1]
+            gender = split_line[2]
+            occupation = split_line[3]
+            zipcode = split_line[4]
+
+            new_user = User(id=user_id, 
+                            age=age, 
+                            gender=gender, 
+                            occupation=occupation, 
+                            zipcode=zipcode)
+            session.add(new_user)
+    session.commit()
 
 def load_movies(session):
-    # use u.item
-    pass
+    user_file = open('seed_data/u.item').read().split('\n')
+    counter = 0
+    for line in user_file:
+        u_line = line.decode('latin1')
+        split_line = u_line.split('|')
+        if len(split_line) >= 4:
+            movie_id = split_line[0]
+            title = split_line[1]
+            date_raw = split_line[2]
+            if date_raw != '':
+                date_format = "%d-%b-%Y"
+                date = datetime.strptime(date_raw, date_format)
+                print date
+            imdb = split_line[3]
+            print counter
+            counter += 1
+
+            new_movie = Movie(id=movie_id,
+                              title=title,
+                              date=date,
+                              imdb=imdb)
+            session.add(new_movie)
+    session.commit()
 
 def load_ratings(session):
     # use u.data
@@ -18,5 +55,5 @@ def main(session):
     pass
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = connect()
     main(s)
